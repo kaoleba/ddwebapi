615b228824e9652aa395488fcdc5e5f495064db0
lkxxzx-wp <lkxxzx-wp@LAPTOP-RAHTO2LT>
2020-03-22 08:22:16 +0800

    新增投票功能

---
+++ Controllers/VoteController.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoteController : ControllerBase
+    {
+        DBHelper db = new DBHelper();
+        [HttpGet]
+        [EnableCors("any")]
+        [Route("VoteInTimeCount")]
+        public string VoteInTimeCount(string dept) {
+            string sql = "select vote_id from vote where vote_dept='"+dept+"' and now() BETWEEN start_time and end_time limit 0,1";
+            return db.GetEntity<string>(sql);
+        }
+
+        [HttpPost]
+        [EnableCors("any")]
+        [Route("SaveVoteInfo")]
+        public int SaveVoteInfo(VoteDetail voteDetail) {
+            voteDetail.vote_detail_id = System.Guid.NewGuid().ToString();
+            voteDetail.vote_detail_def2 = DateTime.Now.ToString();
+            db.Insert<VoteDetail>(voteDetail);
+            return 1;
+        }
+
+        [HttpGet]
+        [EnableCors("any")]
+        [Route("CheckSubmit")]
+        public int CheckSubmit(string userid) {
+            string sql = "select count(*) from vote_detail where vote_detail_def1='"+userid+"'";
+            return db.GetEntity<int>(sql);
+        }
+
+        [HttpGet]
+        [EnableCors("any")]
+        [Route("report")]
+        public List<report> Report()
+        {
+            string sql = "select vote_dept,vote_detail_name,count(*) count from vote_detail a left join vote b on a.vote_id=b.vote_id group by vote_dept,vote_detail_name";
+            return db.GetList<report>(sql);
+        }
+
+    }
+}
---
+++ Models/Vote.cs
@@ -0,0 +1,42 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDWebApi
+{
+    [Table("vote_detail")]
+    //投票活动
+    public class Vote
+    {
+        public string vote_id { get; set; }
+        public string vote_title { get; set; }
+        public string start_time { get; set; }
+        public string end_time { get; set; }
+        public string vote_dept { get; set; }
+    }
+
+    //投票详情
+    [Table("vote_detail")]
+    public class VoteDetail {
+        public string vote_detail_id { get; set; }
+        public string vote_id { get; set; }
+        public string vote_detail_name { get; set; }
+        public string vote_detail_duty { get; set; }
+        public string vote_detail_info { get; set; }
+        public int illegal_flag { get; set; }
+        public string illegal_info { get; set; }
+        public string vote_detail_def1 { get; set; }
+        public string vote_detail_def2 { get; set; }
+        public string vote_detail_def3 { get; set; }
+    }
+
+    public class report {
+        public string vote_dept { get; set; }
+        public string vote_detail_name { get; set; }
+        public int count { get; set; }
+    }
+
+
+}
